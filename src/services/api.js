3b76a601cc35658ae3d52ef5189092c75295d0f0
Kevin-humberto <kevin.huaman.vilcapuma@vallegrande.edu.pe>
2025-10-28 22:13:19 -0500

    Feat url estudent

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = ''; 
+const API_BASE_URL = 'http://localhost:8000'; 
 
 const api = axios.create({
 
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,2 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        // http://localhost:8000
-      
-        target: 'http://localhost:8000', 
-        changeOrigin: true, 
-      },
-    }
-  }
-})
+
